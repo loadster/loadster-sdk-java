504c74af51a23d05344b2735e37bd1fa070b810a
Andy Hawkes <andy@andyhawkes.com>
2016-06-23 08:56:30 -0700

    Switch to Feign + Jackson.
    This is simpler and has fewer transitive dependencies than the old approach.

--- src/main/java/loadster/sdk/client/WorkbenchApiClient.java
+++ src/main/java/loadster/sdk/client/WorkbenchApiClient.java
@@ -1,113 +1,81 @@
 package loadster.sdk.client;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+import feign.*;
+import feign.jackson.JacksonDecoder;
+import feign.jackson.JacksonEncoder;
 import loadster.sdk.exceptions.ApiException;
-import loadster.sdk.types.ErrorDetail;
-import loadster.sdk.types.Reference;
+import loadster.sdk.types.Scenario;
+import loadster.sdk.types.Test;
 import loadster.sdk.types.TestStatistics;
 import loadster.sdk.types.TestStatus;
-import org.apache.http.HttpResponse;
-import org.apache.http.ProtocolException;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
 
 /**
  * Simplified client interface for the Workbench API.
  */
 public class WorkbenchApiClient {
-    private String baseUrl;
-    private String apiKey;
+    private WorkbenchApi jsonApi;
 
-    private DefaultHttpClient httpClient = new DefaultHttpClient();
-    private Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd'T'hh:mm:ss.SSS'Z'").create();
-
-    public WorkbenchApiClient(String host, int port, String apiKey) {
-        this.baseUrl = "http://" + host + ":" + port;
-        this.apiKey = apiKey;
+    public WorkbenchApiClient(final String host, final int port, final String apiKey) {
+        jsonApi = Feign.builder().decoder(new JacksonDecoder()).encoder(new JacksonEncoder()).requestInterceptor(new RequestInterceptor() {
+            @Override
+            public void apply(RequestTemplate requestTemplate) {
+                requestTemplate.query("apiKey", apiKey);
+            }
+        }).target(WorkbenchApi.class, "http://" + host + ":" + port);
     }
 
     /**
-     * Attempts to start a test for a scenario, such as <code>/projects/1/scenarios/3</code>.
+     * Attempts to start a test for a scenario.
      */
-    public Reference startTest(String scenarioUri) throws ApiException, IOException, ProtocolException {
-        HttpPost post = new HttpPost(baseUrl + scenarioUri + "/tests?apiKey=" + apiKey);
-        HttpResponse response = httpClient.execute(post);
-        Reader reader = new InputStreamReader(response.getEntity().getContent(), "UTF-8");
-        int status = response.getStatusLine().getStatusCode();
+    public Test startTest(Scenario scenario) throws ApiException, IOException {
+        Test test = jsonApi.startTest(scenario.getProjectId(), scenario.getId());
 
-        if (status == 201) {
-            return gson.fromJson(reader, Reference.class);
-        } else if (status == 403) {
-            EntityUtils.consumeQuietly(response.getEntity());
+        // TODO - the API should be including these fields in the response
+        test.setProjectId(scenario.getProjectId());
+        test.setScenarioId(scenario.getId());
 
-            throw new ApiException(gson.fromJson(reader, ErrorDetail.class));
-        } else {
-            EntityUtils.consumeQuietly(response.getEntity());
-
-            throw new ApiException(response.getStatusLine().toString());
-        }
+        return test;
     }
 
     /**
      * Gets up-to-date status on a test.
      */
-    public TestStatus getTestStatus(Reference test) throws ApiException, IOException, ProtocolException {
-        HttpGet request = new HttpGet(test.getHref() + "/status?apiKey=" + apiKey);
-        HttpResponse response = httpClient.execute(request);
-        Reader reader = new InputStreamReader(response.getEntity().getContent(), "UTF-8");
-        int status = response.getStatusLine().getStatusCode();
-
-        if (status == 200) {
-            return gson.fromJson(reader, TestStatus.class);
-        } else {
-            EntityUtils.consumeQuietly(response.getEntity());
-
-            throw new ApiException(response.getStatusLine().toString());
-        }
+    public TestStatus getTestStatus(Test test) throws ApiException, IOException {
+        return jsonApi.getTestStatus(test.getProjectId(), test.getScenarioId(), test.getId());
     }
 
     /**
      * Fetches an HTML test report for a test. The test has to be finished or this will fail.
      */
-    public InputStream getTestReport(Reference test) throws ApiException, IOException, ProtocolException {
-        HttpGet request = new HttpGet(test.getHref() + "/report?apiKey=" + apiKey);
-        HttpResponse response = httpClient.execute(request);
-        int status = response.getStatusLine().getStatusCode();
-
-        if (status == 200) {
-            return response.getEntity().getContent();
-        } else {
-            EntityUtils.consumeQuietly(response.getEntity());
-
-            throw new ApiException(response.getStatusLine().toString());
-        }
+    public InputStream getTestReport(Test test) throws ApiException, IOException {
+        return jsonApi.getTestReport(test.getProjectId(), test.getScenarioId(), test.getId()).body().asInputStream();
     }
 
     /**
      * Fetches statistics for a test. The test has to be finished or this will fail.
      */
-    public TestStatistics getTestStatistics(Reference test) throws ApiException, IOException, ProtocolException {
-        HttpGet request = new HttpGet(test.getHref() + "/report?apiKey=" + apiKey);
+    public TestStatistics getTestStatistics(Test test) throws ApiException, IOException {
+        return jsonApi.getTestStatistics(test.getProjectId(), test.getScenarioId(), test.getId());
+    }
 
-        request.setHeader("Accept", "application/json");
+    private interface WorkbenchApi {
+        @RequestLine("POST /projects/{projectId}/scenarios/{scenarioId}/tests")
+        @Headers("Accept: application/json")
+        Test startTest(@Param("projectId") String projectId, @Param("scenarioId") String scenarioId);
 
-        HttpResponse response = httpClient.execute(request);
-        int status = response.getStatusLine().getStatusCode();
+        @RequestLine("GET /projects/{projectId}/scenarios/{scenarioId}/tests/{testId}/status")
+        @Headers("Accept: application/json")
+        TestStatus getTestStatus(@Param("projectId") String projectId, @Param("scenarioId") String scenarioId, @Param("testId") String testId);
 
-        if (status == 200) {
-            return gson.fromJson(new InputStreamReader(response.getEntity().getContent()), TestStatistics.class);
-        } else {
-            EntityUtils.consumeQuietly(response.getEntity());
+        @RequestLine("GET /projects/{projectId}/scenarios/{scenarioId}/tests/{testId}/report")
+        @Headers("Accept: application/json")
+        TestStatistics getTestStatistics(@Param("projectId") String projectId, @Param("scenarioId") String scenarioId, @Param("testId") String testId);
 
-            throw new ApiException(response.getStatusLine().toString());
-        }
+        @RequestLine("GET /projects/{projectId}/scenarios/{scenarioId}/tests/{testId}/report")
+        @Headers("Accept: text/html")
+        Response getTestReport(@Param("projectId") String projectId, @Param("scenarioId") String scenarioId, @Param("testId") String testId);
     }
 }
--- src/main/java/loadster/sdk/tasks/RunTest.java
+++ src/main/java/loadster/sdk/tasks/RunTest.java
@@ -2,50 +2,49 @@ package loadster.sdk.tasks;
 
 import loadster.sdk.client.WorkbenchApiClient;
 import loadster.sdk.exceptions.ApiException;
-import loadster.sdk.types.Reference;
+import loadster.sdk.types.Scenario;
+import loadster.sdk.types.Test;
 import loadster.sdk.types.TestStatus;
-import org.apache.commons.io.IOUtils;
-import org.apache.commons.lang.StringUtils;
-import org.apache.http.ProtocolException;
-import org.apache.log4j.Logger;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 
 /**
  * Simple task to kick off a test via the Loadster Workbench web service, monitor it while it runs, and automatically
  * download a test report once it is finished.
  */
 public class RunTest implements Runnable {
-    private static final Logger log = Logger.getLogger(RunTest.class);
-
     private WorkbenchApiClient client;
-    private String scenarioUri;
+    private String projectId;
+    private String scenarioId;
     private String testReportOutputFile;
 
-    public RunTest(String apiHost, int apiPort, String apiKey, String scenarioUri, String testReportOutputFile) {
+    public RunTest(String apiHost, int apiPort, String apiKey, String projectId, String scenarioId, String testReportOutputFile) {
         this.client = new WorkbenchApiClient(apiHost, apiPort, apiKey);
-        this.scenarioUri = scenarioUri;
+        this.projectId = projectId;
+        this.scenarioId = scenarioId;
         this.testReportOutputFile = testReportOutputFile;
     }
 
     public void run() {
         try {
-            Reference ref = client.startTest(scenarioUri);
+            Scenario scenario = new Scenario(projectId, scenarioId);
+            Test test = client.startTest(scenario);
 
-            log.info("Started test " + ref.getId() + " for scenario " + scenarioUri);
+            log("Started test " + test.getId() + " for scenario " + scenarioId + " of project " + projectId);
 
-            waitForTestToStart(ref);
-            pollWhileTestIsRunning(ref);
+            waitForTestToStart(test);
+            pollWhileTestIsRunning(test);
 
-            log.info("Attempting to fetch the test report...");
+            log("Attempting to fetch the test report...");
 
-            IOUtils.copy(waitForTestReport(ref), new FileOutputStream(testReportOutputFile));
+            copy(waitForTestReport(test), new FileOutputStream(testReportOutputFile));
 
-            log.info("Saved test report to " + testReportOutputFile);
+            log("Saved test report to " + testReportOutputFile);
         } catch (Exception e) {
-            log.error(e);
+            log(e.getMessage());
 
             System.exit(2);
         }
@@ -53,44 +52,60 @@ public class RunTest implements Runnable {
         System.exit(0);
     }
 
-    private void waitForTestToStart(Reference ref) throws ApiException, InterruptedException, IOException, ProtocolException {
+    private void waitForTestToStart(Test test) throws ApiException, InterruptedException, IOException {
         for (int i = 0; i < 60; i++) {
             Thread.sleep(10000);
 
-            TestStatus status = client.getTestStatus(ref);
+            TestStatus status = client.getTestStatus(test);
 
             if (status.getStatus().equals(TestStatus.RUNNING)) {
-                log.info("Test is up and running!");
+                log("Test is up and running!");
 
                 break;
             } else {
-                log.debug("Waiting for test to start...");
+                log("Waiting for test to start...");
             }
         }
     }
 
-    private void pollWhileTestIsRunning(Reference ref) throws ApiException, InterruptedException, IOException, ProtocolException {
-        for (TestStatus status = client.getTestStatus(ref); status.getStatus().equals(TestStatus.RUNNING); status = client.getTestStatus(ref)) {
-            log.info("Test is running (v-users=" + status.getRunningUsers() + ")");
+    private void pollWhileTestIsRunning(Test test) throws ApiException, InterruptedException, IOException {
+        for (TestStatus status = client.getTestStatus(test); status.getStatus().equals(TestStatus.RUNNING); status = client.getTestStatus(test)) {
+            log("Test is running (v-users=" + status.getRunningUsers() + ")");
 
             Thread.sleep(10000);
         }
 
-        log.info("Test is finished!");
+        log("Test is finished!");
     }
 
-    private InputStream waitForTestReport(Reference ref) throws ApiException {
+    private InputStream waitForTestReport(Test test) throws ApiException {
         for (int i = 0; i < 60; i++) {
             try {
                 Thread.sleep(3000);
 
-                return client.getTestReport(ref);
+                return client.getTestReport(test);
             } catch (Exception e) {
-                log.info("Waiting for the test report to become available...");
+                log("Waiting for the test report to become available...");
             }
         }
 
         throw new ApiException("Failed to load the test report!");
+    }
+
+    private static void log(String message) {
+        System.out.println(message);
+    }
+
+    private static void copy(InputStream source, OutputStream destination) throws IOException {
+        byte[] buf = new byte[2048];
+        int x;
+
+        while ((x = source.read(buf)) != -1) {
+            destination.write(buf, 0, x);
+        }
+
+        source.close();
+        destination.close();
     }
 
     public static void main(String[] args) {
@@ -98,17 +113,17 @@ public class RunTest implements Runnable {
         String apiHost = System.getProperty("loadster.api.host");
         int apiPort = Integer.parseInt(System.getProperty("loadster.api.port", "1999"));
 
-        if (StringUtils.isEmpty(apiKey)) {
+        if (apiKey == null || apiKey.length() == 0) {
             System.err.println("API key not set! Use -Dloadster.api.key=changeme");
             System.exit(1);
-        } else if (StringUtils.isEmpty(apiHost)) {
+        } else if (apiHost == null || apiHost.length() == 0) {
             System.err.println("API host not set! Use -Dloadster.api.host=10.0.0.1");
             System.exit(1);
-        } else if (args.length < 2) {
-            System.err.println("Usage: " + RunTest.class.getName() + " /projects/00001/scenarios/00001 report.html");
+        } else if (args.length < 3) {
+            System.err.println("Usage: " + RunTest.class.getName() + " <projectId> <scenarioId> <testReportOutputFile>");
             System.exit(1);
         }
 
-        new RunTest(apiHost, apiPort, apiKey, args[0], args[1]).run();
+        new RunTest(apiHost, apiPort, apiKey, args[0], args[1], args[2]).run();
     }
 }
